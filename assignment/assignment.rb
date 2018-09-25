d2e2a4893ba89e7ad0b10ca230c9e3324104f1ae
chen <chen.alkabets@nova-lumos.com>
2018-09-25 15:43:27 +0300

    rq06.1

--- assignment/assignment.rb
+++ assignment/assignment.rb
@@ -63,6 +63,7 @@ class Assignment
   # Get rows from DB by PK
   #
   def get_user_byid(id)
+    User.where(id: id)
       # accept an id input parameter
       # use the User Model class to get the User associated with the `id` primary key
       # return the User instance that matches the provided id
