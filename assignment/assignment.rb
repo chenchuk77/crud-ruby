f9f5aeb5f06a3bd3f6d9f10bc2566b45b8693b6d
chen <chen.alkabets@nova-lumos.com>
2018-09-25 15:04:54 +0300

    rq04.2

--- assignment/assignment.rb
+++ assignment/assignment.rb
@@ -34,6 +34,7 @@ class Assignment
   end
 
   def find_alllists(offset, limit)
+    TodoList.offset(offset).limit(limit).order(list_due_date: :desc)
       # accept offset and limit input parameters
       # use the TodoList Model class to find all TodoLists, ordered by `list_due_date` descending, with specified row offset and row limit
       # return a collection of TodoList instances that represent the specified rows
