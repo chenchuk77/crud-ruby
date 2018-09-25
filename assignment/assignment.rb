2a5cf45f2f41933f1bfd98509efb49c711695790
chenchuk <chenchuk@gmail.com>
2018-09-25 22:03:53 +0300

    rq07.1

--- assignment/assignment.rb
+++ assignment/assignment.rb
@@ -63,13 +63,14 @@ class Assignment
   # Get rows from DB by PK
   #
   def get_user_byid(id)
-    User.where(id: id)
+      User.where(id: id).first
       # accept an id input parameter
       # use the User Model class to get the User associated with the `id` primary key
       # return the User instance that matches the provided id
   end
 
   def get_todolist_byid(id)
+      TodoList.where(id: id).first
       # accept an id input parameter
       # use the TodoList Model class to get the TodoList associated with the `id` primary key
       # return the TodoList instance that matches the provided id
@@ -79,6 +80,9 @@ class Assignment
   # Update rows in DB
   #
   def update_password(id, password_digest)
+      user = get_user_byid(id)
+      user.password_digest = password_digest
+      user.save
       # accept an id and password_digest input parameters
       # use the User Model class to update the `password_digest` for the User associated with the id primary key
       # (no return is required)
